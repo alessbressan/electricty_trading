e0a00bb45db5424fde4ca88a0f9a77baf8791a92
Alessio <alebre16@gmail.com>
2025-03-10 20:48:08 -0400

    added weather to DataScrapping

--- data_scrapping.py
+++ data_scrapping.py
@@ -1,3 +1,7 @@
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
 import requests
 import zipfile
 import io
@@ -13,10 +17,10 @@ class DataScrapping:
     
     def get_features(self):
         self.update_features(self.rt_prices(), name= 'RT Prices')
-        self.update_features(self.da_prices(), name= 'DA Prices')
-        # # self.weather_forecast()
-        self.update_features(self.load_forecast(), name= 'Load Forecast')
-        self.update_features(self.capacity(), name= 'Capacity')
+        # self.update_features(self.da_prices(), name= 'DA Prices')
+        self.update_features(self.weather_forecast_api(), name= 'Weather')
+        # self.update_features(self.load_forecast(), name= 'Load Forecast')
+        # self.update_features(self.capacity(), name= 'Capacity')
     
     def update_features(self, new_feature:pd.Series, name:str):
         new_feature = new_feature.rename(name)
@@ -159,8 +163,65 @@ class DataScrapping:
         day_ahead.set_index('Time Stamp', inplace= True)
 
         return day_ahead['LBMP ($/MWHr)']
+    
+    def weather_forecast_api(self):
+        # Setup the Open-Meteo API client with cache and retry on error
+        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
 
-    def weather_forecast(self):
+        # Make sure all required weather variables are listed here
+        # The order of variables in hourly or daily is important to assign them correctly below
+        start_date = pd.to_datetime(self.start_year, format="%Y%m%d")
+        end_date = start_date + pd.DateOffset(years= self.n_years)
+        url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": 40.8168,
+            "longitude": -73.0662,
+            "start_date": f"{start_date.strftime("%Y-%m-%d")}",
+            "end_date": f"{end_date.strftime("%Y-%m-%d")}",
+            "hourly": ["temperature_2m", "weather_code", "relative_humidity_2m", "precipitation", "wind_speed_10m", "wind_direction_10m"]
+        }
+        responses = openmeteo.weather_api(url, params=params)
+
+        # Process first location. Add a for-loop for multiple locations or weather models
+        response = responses[0]
+        # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+        # print(f"Elevation {response.Elevation()} m asl")
+        # print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
+        # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+        # Process hourly data. The order of variables needs to be the same as requested.
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+        hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
+        hourly_precipitation = hourly.Variables(3).ValuesAsNumpy()
+        hourly_wind_speed_10m = hourly.Variables(4).ValuesAsNumpy()
+        hourly_wind_direction_10m = hourly.Variables(5).ValuesAsNumpy()
+
+        hourly_data = {"date": pd.date_range(
+            start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+            end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+            freq = pd.Timedelta(seconds = hourly.Interval()),
+            inclusive = "left"
+        )}
+
+        hourly_data["temperature_2m"] = hourly_temperature_2m
+        hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+        hourly_data["precipitation"] = hourly_precipitation
+        hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+        hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+        hourly_data['date'] = hourly_data['date'].to_series().dt.strftime("%Y-%m-%d %H:%M:%S")
+
+        hourly_weather = pd.DataFrame(data = hourly_data)
+        hourly_weather.set_index('date', inplace= True)
+
+        # hourly_weather['HDD'] = hourly_weather.apply(lambda x: self.compute_HDD(max_temp= x['temperature_2m'], min_temp= x['temperature_2m']), axis= 1)
+        # hourly_weather['CDD'] = hourly_weather.apply(lambda x: self.compute_CDD(max_temp= x['temperature_2m'], min_temp= x['temperature_2m']), axis= 1)
+        
+        return hourly_weather["temperature_2m"]
+
+    def weather_forecast_nyiso(self):
         df_year = []
 
         for i in range(12*self.n_years):
