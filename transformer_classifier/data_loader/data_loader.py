011178d1ec1d7dc7c1bfd96fdb08a764d39383b4
Alessio <alebre16@gmail.com>
2025-03-19 14:29:51 -0400

    update

--- transformer_classifier/data_loader/data_loader.py
+++ transformer_classifier/data_loader/data_loader.py
@@ -15,39 +15,26 @@ root_path = config["root_path"]
 data_path = config["data_path"]
 
 class DartDataset():
-    def __init__(self, batch_size, target:str = 'spike_30', device= 'cpu')  :
+    def __init__(self, batch_size, target:str = 'spike_30', device= 'cuda')  :
         super().__init__()
         df_raw = pd.read_csv(os.path.join(root_path, data_path))
         cols_data = df_raw.columns[1:]
         features = df_raw[cols_data]
 
         X = features.iloc[:,:-1].values # removes date value
-        X = (X - X.mean()) / (X.std() + 1e-8)
-        Y = features.loc[:,target].astype(dtype=int).values
+        self.X = (X - X.mean()) / (X.std() + 1e-8)
+        self.Y = features.loc[:,target].astype(dtype=int).values
         
-        dataset = TensorDataset(torch.Tensor(X).to(device), torch.Tensor(Y).to(device))
+        dataset = TensorDataset(torch.Tensor(self.X).to(device), torch.Tensor(self.Y).to(device))
         self.dataloader = DataLoader(dataset, batch_size= batch_size, drop_last=True) 
     
     def getDataLoader(self): 
         return self.dataloader
-
     
+    def getData(self):
+        return self.X, self.Y
 
-class MyTestDataLoader():
-    def __init__(self, batch_size) -> None:
-        self.batch_size = batch_size 
-        df_raw = pd.read_csv(os.path.join(root_path, data_path))
-        cols_data = df_raw.columns[1:]
-        file_out_test = df_raw[cols_data]
 
-        x_test = file_out_test.iloc[:,:-1].values
-        y_test = file_out_test.iloc[:,-1:].astype(dtype=int).values   
- 
-        test_set= DartDataset(x= x_test, y= y_test) 
-        self.dataLoader= DataLoader(test_set, batch_size=self.batch_size, shuffle=True,  ) 
-
-    def getDataLoader(self): 
-        return self.dataLoader
 
 class myDataLoader():
     def __init__(self, batch_size) -> None:
